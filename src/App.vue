ce7f9b5bdd3d2185b956ecb87fe87eddd6a645b8
ec98 <carlospiruch98@gmail.com>
2024-01-11 19:48:02 -0500

    examen_pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,105 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>Puntaje: {{ puntaje }}</p>
+    <p>Intento: {{ intento }}</p>
+
+    <Pokemon :imagen="img1" texto="answer"></Pokemon>
+    <Pokemon :imagen="img2" texto="answer"></Pokemon>
+    <Pokemon :imagen="img3" texto="answer"></Pokemon>
+
+    <button @click="consumirAPI()">JUGAR</button>
+
+    <div v-if="intento >= 5 && puntaje <= 10">
+      <p>Has utilizado tus {{ intento }} intentos</p>
+      <p>El juego ha terminado, intentalo nuevamente</p>
+      <button @click="reiniciar()">Nuevo Juego</button>
+    </div>
+
+    <div v-if="puntaje >= 10">
+      <p>Puntaje: {{ puntaje }}</p>
+      <p>Felicitaciones has ganado un premio de $10.000,00</p>
+      <button @click="reiniciar()">Nuevo Juego</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pokemon from "./components/Pokemon.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Pokemon
+  },
+  methods: {
+    async getAPI() {
+      const get = await fetch("https://yesno.wtf/api").then(p => p.json());
+      this.answer = get.answer;
+      // this.image = get.image;
+      if(this.answer === 'no'){
+        this.answer = "no";
+      } else{
+        this.answer = "yes";
+      }
+      if (this.intento === 0) {
+        this.recibir = 0;
+      }
+      if (this.answer === "yes") {
+        this.recibir++;
+        if (this.recibir === 3) {
+          this.puntaje += 5;
+        } else if (this.recibir === 2) {
+          this.puntaje += 2;
+        } else if (this.recibir === 1) {
+          this.puntaje += 1;
+        }
+      }
+
+      this.img1 = get.image;
+      this.img2 = get.image;
+      this.img3 = get.image;
+    },
+
+    consumirAPI() {
+      this.getAPI();
+      this.puntajeIntento();
+    },
+
+    puntajeContar() {
+      return this.puntaje++;
+    },
+    puntajeIntento() {
+      return this.intento++;
+    },
+    reiniciar(){
+      this.img1 = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg";
+      this.img2 = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg";
+      this.img3 = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg";
+      this.puntaje = 0;
+      this.intento = 0;
+      this.recibir = 0;
+    }
+  },
+  watch: {
+    answer() {
+      this.getAPI();
+    }
+  },
+  data() {
+    return {
+      img1:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
+      img2:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg",
+      img3:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg",
+      puntaje: 0,
+      intento: 0,
+      recibir: 0,
+      answer: null
+    };
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="pokemon">
+    <img :src="imagen" :alt="texto" />
+    <p>{{ texto }}</p>
+  </div>
+</template>
+  
+  <script>
+export default {
+  props: {
+    imagen: String,
+    texto: String
+  }
+};
+</script>
+  
+  <style scoped>
+.pokemon {
+  display: grid;
+  grid-template-rows: 100px 4px;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin: 10px;
+}
+
+.pokemon img {
+  width: 100px; /* Ajusta el tamaño de la imagen según tus preferencias */
+  height: auto;
+  margin: 5px; /* Espaciado entre las imágenes */
+}
+
+.pokemon p {
+  font-size: 14px;
+  color: #333; /* Color del texto */
+  margin: 0;
+}
+</style>
+  
